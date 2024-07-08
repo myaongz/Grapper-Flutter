a6b2549faf957474efe8db6736eb3077b43fbb64
parkyuhyeon <pyh1111802@naver.com>
2024-07-08 16:44:15 +0900

    :bug: :: import 오류 해결

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:grapper/views/recommend_page.dart';
+import 'package:grapper/views/recommend/recommend_page.dart';
 
 void main() {
   runApp(const MyApp());
