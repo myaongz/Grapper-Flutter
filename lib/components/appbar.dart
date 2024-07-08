534f93dafa01f65e08191ea2439bff5a6baa63f5
parkyuhyeon <pyh1111802@naver.com>
2024-07-08 16:16:10 +0900

    📝 :: appbar 컴포넌트로 구축

---
+++ lib/components/appbar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:grapper/utils/grapper_font.dart';
+import 'package:grapper/utils/grapper_icon.dart';
+
+class GrapperAppbar extends StatelessWidget {
+  const GrapperAppbar({super.key, required this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GrapperText(
+            text: text,
+            grapperFont: GrapperFonts.pretendard_700,
+            size: 24,
+            color: Colors.white,
+          ),
+          Container(
+            child: const Row(
+              children: [
+                GrapperIcon(
+                  icon: GrapperIcons.bell,
+                  color: Colors.white,
+                  size: 24,
+                ),
+                SizedBox(
+                  width: 22,
+                ),
+                GrapperIcon(
+                  icon: GrapperIcons.user,
+                  color: Colors.white,
+                  size: 24,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
