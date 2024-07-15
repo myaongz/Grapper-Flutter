3336741bcddb065319baf98c7724b99db0b6a1e7
parkyuhyeon <pyh1111802@naver.com>
2024-07-15 11:08:42 +0900

    :bug: :: import error 해결

--- lib/views/recommend/recommend_page.dart
+++ lib/views/recommend/recommend_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:grapper/components/appbar.dart';
 import 'package:grapper/components/game_button.dart';
