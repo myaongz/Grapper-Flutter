d8f57dea9a82452ada705e4a88ff0121c62e8109
parkyuhyeon <pyh1111802@naver.com>
2024-07-17 00:33:35 +0900

    :bug: :: singlechildscrollview 오류 해결

--- lib/views/recommend/recommend_page.dart
+++ lib/views/recommend/recommend_page.dart
@@ -58,136 +58,138 @@ class RecommendPage extends StatelessWidget {
       body: SafeArea(
         left: false,
         right: false,
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: <Widget>[
-                    GrapperAppbar(
-                      text: '테스트',
-                    ),
-                    SizedBox(
-                      height: 25,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        GrapperHashtag(text: '핫딜'),
-                        GrapperHashtag(
-                          text: '추천',
-                          isSelected: true,
-                        ),
-                        GrapperHashtag(text: '인디게임'),
-                        GrapperHashtag(text: '매드무비'),
-                      ],
-                    ),
-                  ],
+        child: Column(
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  GrapperAppbar(
+                    text: '테스트',
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      GrapperHashtag(text: '핫딜'),
+                      GrapperHashtag(
+                        text: '추천',
+                        isSelected: true,
+                      ),
+                      GrapperHashtag(text: '인디게임'),
+                      GrapperHashtag(text: '매드무비'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 12, bottom: 25),
+              decoration: const BoxDecoration(color: Colors.white),
+              height: 50,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          GrapperText(
+                            text: '이런 게임은 어떠세요?',
+                            grapperFont: GrapperFonts.pretendard_600,
+                            size: 20,
+                            color: Colors.white,
+                          ),
+                          GrapperText(
+                            text: '더보기',
+                            grapperFont: GrapperFonts.pretendard_500,
+                            size: 12,
+                            color: GrapperColorGrey.lightActive,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          GameButton(
+                            gameName: 'PUBG: BATTLEGRONDS',
+                            price: '15,400',
+                            icon: GrapperIcon(
+                              icon: GrapperIcons.steam,
+                              color: Color(0xff5f5f5f),
+                              size: 16,
+                            ),
+                            isDiscount: false,
+                          ),
+                          GameButton(
+                            gameName: 'OVERWATCH',
+                            price: '9,400',
+                            icon: GrapperIcon(
+                              icon: GrapperIcons.epicgames,
+                              color: Color(0xff5f5f5f),
+                              size: 16,
+                            ),
+                            isDiscount: true,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 25),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          GrapperText(
+                            text: '요즘 핫한 인디게임',
+                            grapperFont: GrapperFonts.pretendard_600,
+                            size: 20,
+                            color: Colors.white,
+                          ),
+                          GrapperText(
+                            text: '더보기',
+                            grapperFont: GrapperFonts.pretendard_500,
+                            size: 12,
+                            color: GrapperColorGrey.lightActive,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          GameButton(
+                            gameName: 'PUBG: BATTLEGRONDS',
+                            price: '무료',
+                            icon: GrapperIcon(
+                              icon: GrapperIcons.steam,
+                              color: Color(0xff5f5f5f),
+                              size: 16,
+                            ),
+                            isDiscount: false,
+                          ),
+                          GameButton(
+                            gameName: 'OVERWATCH',
+                            price: '무료',
+                            icon: GrapperIcon(
+                              icon: GrapperIcons.epicgames,
+                              color: Color(0xff5f5f5f),
+                              size: 16,
+                            ),
+                            isDiscount: false,
+                          ),
+                        ],
+                      ),
+                      space(),
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.only(top: 12, bottom: 25),
-                decoration: const BoxDecoration(color: Colors.white),
-                height: 50,
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        GrapperText(
-                          text: '이런 게임은 어떠세요?',
-                          grapperFont: GrapperFonts.pretendard_600,
-                          size: 20,
-                          color: Colors.white,
-                        ),
-                        GrapperText(
-                          text: '더보기',
-                          grapperFont: GrapperFonts.pretendard_500,
-                          size: 12,
-                          color: GrapperColorGrey.lightActive,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 12),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        GameButton(
-                          gameName: 'PUBG: BATTLEGRONDS',
-                          price: '15,400',
-                          icon: GrapperIcon(
-                            icon: GrapperIcons.steam,
-                            color: Color(0xff5f5f5f),
-                            size: 16,
-                          ),
-                          isDiscount: false,
-                        ),
-                        GameButton(
-                          gameName: 'OVERWATCH',
-                          price: '9,400',
-                          icon: GrapperIcon(
-                            icon: GrapperIcons.epicgames,
-                            color: Color(0xff5f5f5f),
-                            size: 16,
-                          ),
-                          isDiscount: true,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 25),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        GrapperText(
-                          text: '요즘 핫한 인디게임',
-                          grapperFont: GrapperFonts.pretendard_600,
-                          size: 20,
-                          color: Colors.white,
-                        ),
-                        GrapperText(
-                          text: '더보기',
-                          grapperFont: GrapperFonts.pretendard_500,
-                          size: 12,
-                          color: GrapperColorGrey.lightActive,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 12),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        GameButton(
-                          gameName: 'PUBG: BATTLEGRONDS',
-                          price: '무료',
-                          icon: GrapperIcon(
-                            icon: GrapperIcons.steam,
-                            color: Color(0xff5f5f5f),
-                            size: 16,
-                          ),
-                          isDiscount: false,
-                        ),
-                        GameButton(
-                          gameName: 'OVERWATCH',
-                          price: '무료',
-                          icon: GrapperIcon(
-                            icon: GrapperIcons.epicgames,
-                            color: Color(0xff5f5f5f),
-                            size: 16,
-                          ),
-                          isDiscount: false,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              space(),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
