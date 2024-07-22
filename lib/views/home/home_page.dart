218e3ecf6c20c3972568b49995f73afd6ecc24ff
parkyuhyeon <pyh1111802@naver.com>
2024-07-23 02:02:38 +0900

    🔥 :: 사용하지않는 mediaquery 코드 삭제

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -19,7 +19,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         left: false,
